da4812fbf78a72cc30e062b8f5081be0c52c8118
AlexandreDSmigon <alexuyea1@gmail.com>
2024-12-04 09:17:11 +0000

    Adicionar controlos à FormUtilizadores

--- GestaoBiblioteca/GUI/FormUtilizadores.Designer.cs
+++ GestaoBiblioteca/GUI/FormUtilizadores.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ListaUtilizadores = new System.Windows.Forms.ListView();
+            this.BotaoCriar = new System.Windows.Forms.Button();
+            this.BotaoEditar = new System.Windows.Forms.Button();
+            this.BotaoEliminar = new System.Windows.Forms.Button();
+            this.BotaoFechar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ListaUtilizadores
+            // 
+            this.ListaUtilizadores.HideSelection = false;
+            this.ListaUtilizadores.Location = new System.Drawing.Point(12, 77);
+            this.ListaUtilizadores.Name = "ListaUtilizadores";
+            this.ListaUtilizadores.Size = new System.Drawing.Size(776, 218);
+            this.ListaUtilizadores.TabIndex = 0;
+            this.ListaUtilizadores.UseCompatibleStateImageBehavior = false;
+            // 
+            // BotaoCriar
+            // 
+            this.BotaoCriar.Location = new System.Drawing.Point(12, 310);
+            this.BotaoCriar.Name = "BotaoCriar";
+            this.BotaoCriar.Size = new System.Drawing.Size(110, 23);
+            this.BotaoCriar.TabIndex = 1;
+            this.BotaoCriar.Text = "&Criar Utilizador";
+            this.BotaoCriar.UseVisualStyleBackColor = true;
+            // 
+            // BotaoEditar
+            // 
+            this.BotaoEditar.Location = new System.Drawing.Point(149, 310);
+            this.BotaoEditar.Name = "BotaoEditar";
+            this.BotaoEditar.Size = new System.Drawing.Size(110, 23);
+            this.BotaoEditar.TabIndex = 2;
+            this.BotaoEditar.Text = "&Editar Utilizador";
+            this.BotaoEditar.UseVisualStyleBackColor = true;
+            // 
+            // BotaoEliminar
+            // 
+            this.BotaoEliminar.Location = new System.Drawing.Point(293, 310);
+            this.BotaoEliminar.Name = "BotaoEliminar";
+            this.BotaoEliminar.Size = new System.Drawing.Size(110, 23);
+            this.BotaoEliminar.TabIndex = 3;
+            this.BotaoEliminar.Text = "E&liminar Utilizador";
+            this.BotaoEliminar.UseVisualStyleBackColor = true;
+            // 
+            // BotaoFechar
+            // 
+            this.BotaoFechar.Location = new System.Drawing.Point(678, 310);
+            this.BotaoFechar.Name = "BotaoFechar";
+            this.BotaoFechar.Size = new System.Drawing.Size(110, 23);
+            this.BotaoFechar.TabIndex = 4;
+            this.BotaoFechar.Text = "&Fechar";
+            this.BotaoFechar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Gestão de Utilizadores";
+            // 
+            // FormUtilizadores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 345);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BotaoFechar);
+            this.Controls.Add(this.BotaoEliminar);
+            this.Controls.Add(this.BotaoEditar);
+            this.Controls.Add(this.BotaoCriar);
+            this.Controls.Add(this.ListaUtilizadores);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FormUtilizadores";
             this.Text = "FormUtilizadores";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView ListaUtilizadores;
+        private System.Windows.Forms.Button BotaoCriar;
+        private System.Windows.Forms.Button BotaoEditar;
+        private System.Windows.Forms.Button BotaoEliminar;
+        private System.Windows.Forms.Button BotaoFechar;
+        private System.Windows.Forms.Label label1;
     }
 }
